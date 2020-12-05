c89a5daf7388b0c20643de730e49bf0393fcfed7
Hannah Olukoye <hannah.olukoye@macro-eyes.com>
2020-12-05 20:08:57 +0300

    Setup files

---
+++ TestMeds/app/src/main/java/com/hannah/testmeds/MainApp.java
@@ -0,0 +1,11 @@
+package com.hannah.testmeds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.os.Bundle;
+
+public class MainApp extends Application {
+
+
+}
